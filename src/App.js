11c92c52c9f2b6121ead10ae3fea887337e226fc
WessamDah <wessamdahrouj@gmail.com>
2025-02-24 23:43:12 +0300

    Updated routing to use HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// GitHub Pages does not support server-side routing, so HashRouter ensures that all routes are loaded correctly.
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 // components
 import MainLayout from "./pages/Main/Main";
@@ -9,22 +16,24 @@ import Unauthorized from "./pages/Unauthorized/Unauthorized";
 
 export function App() {
   return (
-    <BrowserRouter basename="/work-culture">
+    // <BrowserRouter basename="/work-culture">
+    <Router>
       <AnimatePresence mode="wait">
         <Routes>
-          {/* public Routes */}
+          {/* Public Routes */}
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/*" element={<Error />} />
           <Route path="/unauthorized" element={<Unauthorized />} />
 
-          {/* private route */}
+          {/* Private Route */}
           <Route path="/dashbord/*" element={<MainLayout />} />
           <Route path="/dashbord" element={<Navigate to="/dashbord/home" />} />
         </Routes>
       </AnimatePresence>
-    </BrowserRouter>
+    </Router>
+    // </BrowserRouter>
   );
 }
 
